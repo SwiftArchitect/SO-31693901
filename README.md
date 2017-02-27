ca5bb716c71cea1a1007e5f7820f6c3bdcd47016
Xavier Schott <assembla@thegothicparty.com>
2017-02-26 21:52:30 -0800

    SO-31693901

---
+++ README.md
@@ -0,0 +1,18 @@
+# Design UITableView's section header in Interface Builder on iOS
+
+---
+
+### Stack Overflow Question 31693901
+Xcode project & source code related to [Storyboard or XIB](http://stackoverflow.com/a/32261262/218152) anwser.
+
+---
+
+**Instructions:**
+
+1. Build & run
+
+**References:**
+
+- SO-31693901 on [Stack Overflow](http://stackoverflow.com/questions/31693901/design-uitableviews-section-header-in-interface-builder)
+- This and other answers posted on [Swift Recipes](http://swiftarchitect.com/recipes/)
+
---
+++ SO-31693901/TableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TableViewController.swift
+//  SO-31693901
+//
+//  Copyright © 2017 Xavier Schott
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.register(UINib(nibName: "XIBSectionHeader", bundle:nil),
+                                forCellReuseIdentifier: "xibheader")
+    }
+
+    //MARK: UITableViewDelegate
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
+    {
+        return (0 == section)
+        ? tableView.dequeueReusableCell(withIdentifier: "header")
+        : tableView.dequeueReusableCell(withIdentifier: "xibheader")
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    //MARK: UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 1
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        return tableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell!
+    }
+}
